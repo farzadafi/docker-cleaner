51ccb18fb0019f9ae839bc3a1b860bdcdadcf620
farzadafi <farzadafi40@gmail.com>
2026-01-30 17:03:52 +0330

    initial commit

---
+++ docker-cleaner/src/main/java/ir/farzadafi/DockerCleanerApplication.java
@@ -0,0 +1,11 @@
+package ir.farzadafi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DockerCleanerApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(DockerCleanerApplication.class, args);
+    }
+}
