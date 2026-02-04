89c89135bef4ab6eb2d121d65810e65cd1ae9742
farzadafi <farzadafi40@gmail.com>
2026-02-04 10:44:34 +0330

    create DockerInstructionMapper for map DockerInstruction model to SemanticDockerInstruction model

---
+++ docker-cleaner/src/main/java/ir/farzadafi/mapper/DockerInstructionMapper.java
@@ -0,0 +1,13 @@
+package ir.farzadafi.mapper;
+
+import ir.farzadafi.dto.DockerInstruction;
+import ir.farzadafi.model.semantic.SemanticDockerInstruction;
+import org.springframework.stereotype.Component;
+
+@Component
+public class DockerInstructionMapper {
+
+    public SemanticDockerInstruction map(DockerInstruction dockerInstruction) {
+        return null;
+    }
+}
