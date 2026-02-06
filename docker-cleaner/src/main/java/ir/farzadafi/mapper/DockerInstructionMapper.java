bf5a55557ab4d219bafd15434d16f5208aadb8bf
farzadafi <farzadafi40@gmail.com>
2026-02-06 11:30:56 +0330

    add two new instruction to map method for add and copy

--- docker-cleaner/src/main/java/ir/farzadafi/mapper/DockerInstructionMapper.java
+++ docker-cleaner/src/main/java/ir/farzadafi/mapper/DockerInstructionMapper.java
@@ -14,18 +14,49 @@ public class DockerInstructionMapper {
     public SemanticDockerInstruction map(DockerInstruction raw) {
         return switch (raw.type()) {
             case "RUN" -> mapRun(raw);
-            default -> null;
+            case "ADD" -> mapAdd(raw);
+            case "COPY" -> mapCopy(raw);
+            default -> mapUnknown(raw);
         };
     }
 
     private RunInstruction mapRun(DockerInstruction raw) {
         List<String> tokens = getTokenValues(raw);
         int line = getLine(raw);
+        if (tokens.isEmpty())
+            return new RunInstruction("", List.of(), line);
+        if (tokens.size() == 1)
+            return new RunInstruction(tokens.getFirst(), List.of(), line);
         return new RunInstruction(
                 tokens.getFirst(),
                 tokens.subList(1, tokens.size()),
                 line
         );
+    }
+
+    private AddInstruction mapAdd(DockerInstruction raw) {
+        List<String> tokens = getTokenValues(raw);
+        int line = getLine(raw);
+        if (tokens.size() < 2)
+            return new AddInstruction("", "", line);
+        return new AddInstruction(tokens.get(0), tokens.get(1), line);
+    }
+
+    private CopyInstruction mapCopy(DockerInstruction raw) {
+        List<String> tokens = getTokenValues(raw);
+        int line = getLine(raw);
+        if (tokens.size() < 2)
+            return new CopyInstruction("", "", line);
+        return new CopyInstruction(tokens.get(0), tokens.get(1), line);
+    }
+
+    private UnknownInstruction mapUnknown(DockerInstruction raw) {
+        int line = 0;
+        try {
+            line = getLine(raw);
+        } catch (Exception ignored) {
+        }
+        return new UnknownInstruction(raw.type(), line);
     }
 
     private List<String> getTokenValues(DockerInstruction raw) {
